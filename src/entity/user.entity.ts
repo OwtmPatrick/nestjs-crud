a6bef09938ca582df0901f1f1f55cdacf4daffc4
Radik Zangirov <owtmpatrick@gmail.com>
2023-03-07 09:15:55 +0500

    add swagger schema properties

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,13 +1,17 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
 
 @Entity({ name: 'user' })
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column({ type: 'varchar', length: 300 })
   name: string;
 
+  @ApiProperty()
   @Column({ type: 'varchar', length: 300 })
   age: number;
 }
